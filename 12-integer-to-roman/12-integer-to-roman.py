132944656ba079e4e8a15a494b9633e72c57b59c
Abhay Rajde <63494673+abhayrajde@users.noreply.github.com>
2022-02-20 02:22:38 -0500

    Time: 32 ms (94.89%), Space: 13.4 MB (41.50%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def intToRoman(self, num):
+        nums = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
+        rom = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"]
+        i = 12
+        op = ""
+        while(num!=0):
+            if(nums[i]<=num):
+                op += rom[i]
+                num -= nums[i]
+            else:
+                i-=1
+        return(op)
+        """
+        :type num: int
+        :rtype: str
+        """
+        
