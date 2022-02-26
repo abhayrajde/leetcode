60c50fe9569cd742181e89e023decef69dd0ed2b
Abhay Rajde <63494673+abhayrajde@users.noreply.github.com>
2022-02-26 02:55:54 -0500

    Time: 327 ms (20.58%), Space: 13.7 MB (9.51%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -24,7 +24,30 @@ class Solution(object):
                 else:
                     return(target)
         return(mintemp)
+        
+        
+        """
+        #FOUND THIS SOLUTION IN DISCUSSION, WHICH IS IS KINDOFF EASIER AND MORE APPROACHABLE
+        #GETTING THE ABSOLUTE VALUE IN BOTH THE WAY MAKES THIS SOLUTION MORE APPROACHABLE AND EASY
+        num.sort()
+        result = num[0] + num[1] + num[2]
+        for i in range(len(num) - 2):
+            j, k = i+1, len(num) - 1
+            while j < k:
+                sum = num[i] + num[j] + num[k]
+                if sum == target:
+                    return sum
                 
+                if abs(sum - target) < abs(result - target):
+                    result = sum
+                
+                if sum < target:
+                    j += 1
+                elif sum > target:
+                    k -= 1
+            
+        return result
+        """        
         """
         :type nums: List[int]
         :type target: int
