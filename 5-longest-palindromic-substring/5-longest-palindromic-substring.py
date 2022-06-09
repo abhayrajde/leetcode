bfe1154004ec726fcb9892a7dca090db3a3ec9a2
Abhay Rajde <63494673+abhayrajde@users.noreply.github.com>
2022-06-09 01:53:31 -0400

    Time: 1671 ms (32.14%), Space: 13.4 MB (87.53%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        # if(len(s) == 1):
+        #     return(s)
+        # else:
+        pal = ""
+        pal_len = 0
+        
+        # In this loop i will be the center of palindrome
+        for mid in range(len(s)):
+            # if (mid == 0):
+            #     pal = s[0]
+            
+            # else:    
+            # for odd palindrome
+            i = mid
+            j = mid
+            while(i>=0 and j<len(s) and s[i] == s[j]):
+                if(j-i+1>pal_len):
+                    pal = s[i:j+1]
+                    pal_len = j-i+1
+                i-=1
+                j+=1
+
+
+            # for even length palindrome
+            i = mid
+            j = mid+1
+            while(i>=0 and j<len(s) and s[i] == s[j]):
+                if(j-i+1>pal_len):
+                    pal = s[i:j+1]
+                    pal_len = j-i+1
+                i-=1
+                j+=1
+        return(pal)
+
+            
+            
+        """
+        :type s: str
+        :rtype: str
+        """
+        
