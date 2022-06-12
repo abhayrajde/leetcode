eb1b7b270963267b626493e42a4fc813e6dbf2e5
Abhay Rajde <63494673+abhayrajde@users.noreply.github.com>
2022-06-12 11:24:51 -0400

    Time: 76 ms (29.97%), Space: 15.8 MB (5.29%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,11 +1,38 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        s = sorted(s)
-        t = sorted(t)
-        if(s == t):
-            return(True)
-        else:
+        #Using HashMap Logic
+        
+        if(len(s)!=len(t)):
             return(False)
+        
+        hms = {}
+        hmt = {}
+        for i in range(len(s)):
+            if(s[i] in hms):
+                hms[s[i]]+=1
+            else:
+                hms[s[i]] = 1
+        
+        for i in range(len(t)):
+            if(t[i] in hmt):
+                hmt[t[i]]+=1
+            else:
+                hmt[t[i]] = 1
+        
+        for i in range(len(s)):
+            if((s[i] not in hmt) or (hms[s[i]] != hmt[s[i]])):
+                return(False)
+        return(True)
+        
+        
+        
+        #Using normal Sorting Technique - Time Complexity = 2nlogn +2n | Space = O(1)
+        # s = sorted(s)
+        # t = sorted(t)
+        # if(s == t):
+        #     return(True)
+        # else:
+        #     return(False)
         """
         :type s: str
         :type t: str
