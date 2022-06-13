50f14028977a7b0c6ccdbe3fa02a797392582963
Abhay Rajde <63494673+abhayrajde@users.noreply.github.com>
2022-06-13 11:33:03 -0400

    Time: 1016 ms (41.68%), Space: 17 MB (23.35%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,63 +1,28 @@
 class Solution(object):
     def threeSum(self, nums):   
         # O(N^2) SOLUTION
-        # nums.sort()
-        # output_list = []
-        # for i in range(len(nums)-2):
-        #     if(i == 0 or (nums[i]!=nums[i-1])):
-        #         low = i+1
-        #         high = len(nums)-1
-        #         sum1 = -(nums[i])
-        #         while(low < high):
-        #             if(nums[low]+nums[high]==sum1):
-        #                 temp = [nums[i],nums[low],nums[high]]
-        #                 output_list.append(temp)
-        #                 while(low<high and (nums[low]==nums[low+1])):
-        #                     low+=1
-        #                 while(low<high and (nums[high-1]==nums[high])):
-        #                     high-=1
-        #                 low+=1
-        #                 high-=1
-        #             elif(nums[low] + nums[high] < sum1):
-        #                 low+=1
-        #             else:
-        #                 high-=1
-        # return(output_list)
-                        
         nums.sort()
-        op = []
+        output_list = []
         for i in range(len(nums)-2):
-            if(i == 0 or nums[i] != nums[i-1]):
+            if(i == 0 or (nums[i]!=nums[i-1])):
                 low = i+1
                 high = len(nums)-1
-                temp1 = -(nums[i])
-                while(low<high):
-                    if(nums[low]+nums[high]==temp1):
-                        op.append([nums[i],nums[low],nums[high]])
-                        while(low<high and nums[low]==nums[low+1]):
+                sum1 = -(nums[i])
+                while(low < high):
+                    if(nums[low]+nums[high]==sum1):
+                        temp = [nums[i],nums[low],nums[high]]
+                        output_list.append(temp)
+                        while(low<high and (nums[low]==nums[low+1])):
                             low+=1
-                        while(low<high and nums[high]==nums[high-1]):
+                        while(low<high and (nums[high-1]==nums[high])):
                             high-=1
                         low+=1
                         high-=1
-                    elif(nums[low]+nums[high]>temp1):
-                        high-=1
-                    else:
+                    elif(nums[low] + nums[high] < sum1):
                         low+=1
-        return(op)
-                    
-                        
-                
-                
-        
-        
-        
-        
-        
-        
-        
-        
-        
+                    else:
+                        high-=1
+        return(output_list)
         
         """
         set1 = set(nums)
