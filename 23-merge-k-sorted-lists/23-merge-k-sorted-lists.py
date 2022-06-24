eebc97b0f5fb2453b9524a880440a3203faa7bd1
Abhay Rajde <63494673+abhayrajde@users.noreply.github.com>
2022-06-23 20:21:12 -0400

    Time: 189 ms (30.18%), Space: 19.1 MB (98.79%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -5,38 +5,39 @@
 #         self.next = next
 class Solution(object):
     def mergeKLists(self, lists):
-        if not lists or len(lists) == 0:
+        
+        if( not lists or len(lists)==0):
             return(None)
         
         while(len(lists)>1):
             mergelist = []
-            
-            for i in range(0, len(lists), 2):
+            for i in range(0,len(lists),2):
                 l1 = lists[i]
-                l2 = lists[i+1] if((i+1)<len(lists)) else None
+                if((i+1 < len(lists))):
+                    l2 = lists[i+1]
+                else:
+                    l2 = None
                 mergelist.append(self.merge(l1,l2))
             lists = mergelist
         return(lists[0])
-        
-        
-        
-        
+    
     def merge(self,l1,l2):
         temp = ListNode()
         op = temp
         while(l1 and l2):
-            if(l1.val>=l2.val):
-                op.next = l2
-                l2 = l2.next
-            else:
+            if(l1.val < l2.val):
                 op.next = l1
                 l1 = l1.next
+            else:
+                op.next = l2
+                l2 = l2.next
             op = op.next
         if(l1):
             op.next = l1
         else:
             op.next = l2
         return(temp.next)
+        #TODO
         """
         :type lists: List[ListNode]
         :rtype: ListNode
