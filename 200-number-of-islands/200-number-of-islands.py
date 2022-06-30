b7306bf7cad1436b4b1f7d8d79fc9070e3d862d8
Abhay Rajde <63494673+abhayrajde@users.noreply.github.com>
2022-06-29 17:20:57 -0700

    Time: 1557 ms (8.93%), Space: 32.4 MB (24.94%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,6 +1,69 @@
 class Solution(object):
     def numIslands(self, grid):
         if(not grid):
+            return(0)
+        
+        rows, cols = len(grid), len(grid[0])
+        islands = 0   # count of islands
+        visited = set()
+        
+        def bfs(r,c):
+            q = deque()
+            q.append((r,c))
+            visited.add((r,c))
+            while(q):
+                row, col = q.popleft()
+                directions = [[1,0],[-1,0],[0,1],[0,-1]]
+                for i,j in directions:
+                    r,c = row+i, col+j
+                    if(r in range(rows) and
+                       c in range(cols) and 
+                       grid[r][c] == "1" and ((r,c) not in visited)):
+                        visited.add((r,c))
+                        q.append((r,c))
+                
+        for r in range(rows):
+            for c in range(cols):
+                if(grid[r][c] == "1" and (r,c) not in visited):
+                    bfs(r,c)
+                    islands+=1
+        return(islands)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        """
+        if(not grid):
             return (0)
         
         rows,cols = len(grid), len(grid[0])
@@ -29,6 +92,7 @@ class Solution(object):
                     bfs(r,c)
                     islands+=1
         return(islands)
+        """
         """
         :type grid: List[List[str]]
         :rtype: int
