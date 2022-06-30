4ddc524754c036131896aace8846868f58a0712e
Abhay Rajde <63494673+abhayrajde@users.noreply.github.com>
2022-06-29 19:09:40 -0700

    Time: 2620 ms (5.02%), Space: 34.8 MB (9.73%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -7,60 +7,28 @@ class Solution(object):
         islands = 0   # count of islands
         visited = set()
         
-        def bfs(r,c):
-            q = deque()
-            q.append((r,c))
-            visited.add((r,c))
-            while(q):
-                row, col = q.popleft()
-                directions = [[1,0],[-1,0],[0,1],[0,-1]]
-                for i,j in directions:
-                    r,c = row+i, col+j
-                    if(r in range(rows) and
-                       c in range(cols) and 
-                       grid[r][c] == "1" and ((r,c) not in visited)):
-                        visited.add((r,c))
-                        q.append((r,c))
-                
+        #Recursive DFS
+        def dfs(r,c):
+            directions = [[1,0],[-1,0],[0,1],[0,-1]]
+            print(r,c)
+            for row,col in directions:
+                dr,dc = row+r, col+c
+                if((dr>=0 and dr<rows) and (dc>=0 and dc<cols) and (dc in range(cols)) 
+                   and ((dr,dc) not in visited) and (grid[dr][dc] == "1")):
+                    visited.add((dr,dc))
+                    
+                    dfs(dr,dc)
+                # return
+            return
+        
         for r in range(rows):
             for c in range(cols):
-                if(grid[r][c] == "1" and (r,c) not in visited):
-                    bfs(r,c)
+                if(grid[r][c] == "1" and ((r,c) not in visited)):
+                    # print("new")
+                    dfs(r,c)
+                    # print(r,c)
                     islands+=1
         return(islands)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         
         """
         if(not grid):
