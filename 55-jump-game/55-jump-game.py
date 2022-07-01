e465434d05c65b105c69095f787f38af7d321718
Abhay Rajde <63494673+abhayrajde@users.noreply.github.com>
2022-06-30 23:03:02 -0700

    Time: 6634 ms (5.28%), Space: 14.8 MB (7.79%) - LeetHub

---
+++ 55-jump-game/55-jump-game.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def canJump(self, nums):
+        reach = [False]*len(nums)
+        # reach[-1] = True
+        for i in range(len(nums)-1,-1,-1):
+            # print(nums[i]+i)
+            if(nums[i]+i >= len(nums)-1):
+                reach[i] = True
+            else:
+                for j in range(i,nums[i]+i+1):
+                    if(reach[j]):
+                        reach[i] = True
+                        break
+        # print(reach)
+        return(reach[0])
+        
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
