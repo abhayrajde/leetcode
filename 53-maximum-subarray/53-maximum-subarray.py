0009d36e7c851bb0a23f963bbb6b7e82b2b107dc
Abhay Rajde <63494673+abhayrajde@users.noreply.github.com>
2022-06-30 21:58:59 -0700

    Time: 591 ms (92.87%), Space: 25.6 MB (46.35%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -4,20 +4,15 @@ class Solution(object):
         left, right = 0,0
         currsum = 0
         while(right<len(nums)):
-            # print(nums[right])
+            
             currsum+=nums[right]
             max1 = max(max1, currsum)
-            # if(currsum > max1):
-            #     max1 = currsum
-            #     right+=1
-                
+            
             if(currsum < 0):
                 left = right+1
-                # right = right+1
                 currsum = 0
+            
             right = right+1
-            # else:
-                # right+=1
         return(max1)
         """
         :type nums: List[int]
