6250d2f1b6194523be4bb39affc8e521d826a404
Abhay Rajde <63494673+abhayrajde@users.noreply.github.com>
2022-07-14 00:55:16 -0400

    Time: 39 ms (44.87%), Space: 13.6 MB (72.30%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,6 +1,5 @@
 class Solution(object):
     def subsetsWithDup(self, nums):
-        
         res = []
         nums.sort()
         self.dfs(nums,[],res)
@@ -12,27 +11,6 @@ class Solution(object):
             if(i>0 and nums[i] == nums[i-1]):
                 continue
             self.dfs(nums[i+1:],curr+[nums[i]], res)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         res = []
-#         nums.sort()
-#         self.dfs(nums, [], res)
-#         return(res)
-        
-#     def dfs(self, nums, curr, res):
-#         res.append(curr)
-#         for i in range(len(nums)):
-#             if(i>0 and nums[i] == nums[i-1]):
-#                 continue
-#             self.dfs(nums[i+1:], curr+[nums[i]], res)
-        
         """
         :type nums: List[int]
         :rtype: List[List[int]]
