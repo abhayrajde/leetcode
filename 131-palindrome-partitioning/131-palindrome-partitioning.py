53db6145614d697626859110aa2b13205f14fc40
Abhay Rajde <63494673+abhayrajde@users.noreply.github.com>
2022-07-14 16:53:24 -0400

    Time: 677 ms (73.92%), Space: 27.2 MB (95.98%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -17,36 +17,6 @@ class Solution(object):
             if(self.ispalindrome(s[:i+1])):
                 self.dfs(s[i+1:], curr+[s[:i+1]], res)
 
-                
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        """
-        res = []
-        self.dfs(s, [], res)
-        return res
-
-    def dfs(self, s, path, res):
-        if not s:
-            res.append(path)
-            return
-        for i in range(1, len(s)+1):
-            if self.isPal(s[:i]):
-                self.dfs(s[i:], path+[s[:i]], res)
-
-    def isPal(self, s):
-        return s == s[::-1]
-        """
         """
         :type s: str
         :rtype: List[List[str]]
