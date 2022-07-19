4cedb95501a10094b4c65ff9c885cea577aaf717
Abhay Rajde <63494673+abhayrajde@users.noreply.github.com>
2022-07-18 23:54:28 -0400

    Time: 612 ms (57.21%), Space: 15.8 MB (13.49%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.py
+++ 621-task-scheduler/621-task-scheduler.py
@@ -1,20 +1,16 @@
 class Solution(object):
     def leastInterval(self, tasks, n):
-        # print(tasks)
         count = {}
         for i in range(len(tasks)):
             if(tasks[i] not in count):
                 count[tasks[i]] = 0
             count[tasks[i]]+=1
-        # print(count)
         
         # maxheap = []
         # for i in range(len(count)):
         #     maxheap.apppend(-(count[i]))
         maxheap = [-cnt for cnt in count.values()]
-        # print(maxheap)
         heapq.heapify(maxheap)
-        # print(maxheap)
         
         time = 0 
         q = deque()     #pair of values[-cnt, idleTime(time at which the value it will be available to add to q)]
