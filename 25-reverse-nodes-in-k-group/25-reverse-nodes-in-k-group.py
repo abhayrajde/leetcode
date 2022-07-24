72a7a0952d53681a479cfa886a56d64163739b2f
Abhay Rajde <63494673+abhayrajde@users.noreply.github.com>
2022-07-24 02:15:30 -0400

    Time: 69 ms (23.42%), Space: 15.2 MB (50.56%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.py
@@ -25,32 +25,6 @@ class Solution(object):
             curr = temp
             count-=1
         return(curr, prev)
-        
-        
-        
-#         class Solution(object):
-#     def reverseKGroup(self, head, k):
-#         count, node = 0, head
-#         while node and count < k:
-#             node = node.next
-#             count += 1
-#         if count < k: return head
-#         new_head, prev = self.reverse(head, count)
-#         head.next = self.reverseKGroup(new_head, k)
-#         return prev
-    
-#     def reverse(self, head, count):
-#         prev, cur, nxt = None, head, head
-#         while count > 0:
-#             nxt = cur.next
-#             cur.next = prev
-#             prev = cur
-#             cur = nxt
-#             count -= 1
-#         return (cur, prev)
-        
-#         def reverse(node):
-            
                 
             
         """
