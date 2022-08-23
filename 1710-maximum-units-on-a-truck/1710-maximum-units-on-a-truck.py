75c3c24404d8155bb1201e4620d2482a65737999
Abhay Rajde <abrajde@iu.edu>
2022-08-23 15:04:41 -0400

    Time: 263 ms (30.14%), Space: 13.8 MB (81.52%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -11,18 +11,6 @@ class Solution(object):
                 res += (truckSize*units)
                 return res
         return res
-        '''
-        boxTypes.sort(key=lambda x: -x[1])
-        boxes = 0
-        for box, units in boxTypes:
-            if truckSize > box:
-                truckSize -= box
-                boxes += box * units
-            else:
-                boxes += truckSize * units
-                return boxes
-        return boxes
-        '''
         """
         :type boxTypes: List[List[int]]
         :type truckSize: int
