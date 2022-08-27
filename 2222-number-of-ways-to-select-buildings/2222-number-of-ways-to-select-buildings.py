8f4592214e195482759c941d737b39de1db299d0
Abhay Rajde <abrajde@iu.edu>
2022-08-26 20:35:18 -0400

    Time: 1263 ms (51.61%), Space: 17.8 MB (50.00%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.py
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.py
@@ -1,5 +1,18 @@
 class Solution(object):
     def numberOfWays(self, s):
+        dp = {"0": 0, "1": 0, "01": 0, "10": 0, "010": 0, "101": 0}
+        for i in range(len(s)):
+            if s[i] == "0":
+                dp["0"] += 1
+                dp["10"] += dp["1"]
+                dp["010"] += dp["01"]
+            if s[i] == "1":
+                dp["1"] += 1
+                dp["01"] += dp["0"]
+                dp["101"] += dp["10"]
+                
+        return dp["010"] + dp["101"]
+        '''
         n = len(s)
         pre0 = [0]*n
         pre1 = [0]*n
@@ -28,7 +41,7 @@ class Solution(object):
                 y = pre0[n-1] - pre0[i]
                 res += (x*y)
         return res
-                
+        '''        
         """
         :type s: str
         :rtype: int
