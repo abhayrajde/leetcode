ba6bcfe0622e1732df407111bc2dd245952b86f0
Abhay Rajde <abrajde@iu.edu>
2022-10-04 21:39:25 -0400

    Time: 23 ms (69.65%), Space: 13.8 MB (5.45%) - LeetHub

---
+++ 394-decode-string/394-decode-string.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def decodeString(self, s):
+        stack = []
+        
+        for i in range(len(s)):
+            if s[i] != "]":
+                stack.append(s[i])
+            
+            else:
+                substr = ""
+                while stack[-1] != "[":
+                    substr = stack.pop() + substr
+                stack.pop()
+            
+                k = ""
+                while(stack and stack[-1].isdigit()):
+                    k = stack.pop() + k
+
+                stack.append(int(k) * substr)
+        return("".join(stack))
+        
+        """
+        :type s: str
+        :rtype: str
+        """
+        
