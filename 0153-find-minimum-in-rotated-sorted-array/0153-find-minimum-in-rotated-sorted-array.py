a8bc889dffa1500ff7883e48ff0c7a7a007b2f71
Abhay Rajde <abrajde@iu.edu>
2022-10-12 03:42:05 -0400

    Time: 28 ms (89.38%), Space: 13.7 MB (54.89%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,39 +1,24 @@
 class Solution(object):
     def findMin(self, nums):
         # 
-#         start = 0
-#         end = len(nums)-1
-#         # mid = (start + end)//2
-#         res = nums[0]
-#         while end > start:
-#             if nums[start] < nums[end]:
-#                 res = min(res,nums[start])
-#                 break
-            
-#             mid = (start + end)//2
-#             res = min(res,nums[mid])
-            
-#             if nums[mid] >= nums[start]:
-#                 start = mid + 1
-            
-#             else:
-#                 end = mid - 1
-#         return res
-        l = 0
-        r = len(nums)-1
+        start = 0
+        end = len(nums)-1
+        # mid = (start + end)//2
         res = nums[0]
-        while(l<=r):
-            if(nums[l] < nums[r]):
-                res = min(res,nums[l])
+        while end >= start:
+            if nums[start] < nums[end]:
+                res = min(res,nums[start])
                 break
             
-            m = (l+r)//2
-            res = min(res,nums[m])
-            if(nums[m]>=nums[l]):
-                l = m + 1
+            mid = (start + end)//2
+            res = min(res,nums[mid])
+            
+            if nums[mid] >= nums[start]:
+                start = mid + 1
+            
             else:
-                r = m - 1
-        return(res)
+                end = mid - 1
+        return res
             
             
             
