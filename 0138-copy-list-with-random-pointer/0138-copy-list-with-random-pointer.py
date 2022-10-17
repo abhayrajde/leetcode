96ad8c63dd5d325c554e40e08e527deb307a5065
Abhay Rajde <abrajde@iu.edu>
2022-10-17 02:17:00 -0400

    Time: 72 ms (56.26%), Space: 14.3 MB (67.59%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -10,13 +10,17 @@ class Node:
 class Solution(object):
     def copyRandomList(self, head):
         oldtocopy = { None:None }
+
         curr = head
+
+        # STEP1: WE WILL MAP THE COPY NODE WITH ACTUAL NODE
         while curr:
             copy = Node(curr.val)
             oldtocopy[curr] = copy
             # copy.next = curr.next
             curr = curr.next
         
+        # STEP2: GET THE COPY NODE FROM THE MAP & ADD NEXT AND RANDOM POINTER SAME AS ACTUAL NODE
         curr = head
         while curr:
             copy = oldtocopy[curr]
