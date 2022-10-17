3b9a559404a9b90d48888064459565f25f193056
Abhay Rajde <abrajde@iu.edu>
2022-10-17 15:21:18 -0400

    Time: 156 ms (76.57%), Space: 18.2 MB (82.24%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def merge(self, intervals):
+        
+        intervals = sorted(intervals, key = lambda x: x[0])
+        i = 0
+        while(i < len(intervals)-1):
+            if intervals[i][1] >= intervals[i+1][0]:
+                
+                intervals[i] = [intervals[i][0], max(intervals[i][1], intervals[i+1][1])]
+                intervals.remove(intervals[i+1])
+            
+            else:
+                i+=1
+            
+        return intervals
+                
+        
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        
