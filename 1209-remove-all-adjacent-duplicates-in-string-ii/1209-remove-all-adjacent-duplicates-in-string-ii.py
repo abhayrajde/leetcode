70b0bcaa75843b88bd527a2d8d84ff60ef9fbeef
Abhay Rajde <abrajde@iu.edu>
2022-10-17 22:18:59 -0400

    Time: 3246 ms (9.90%), Space: 28.4 MB (5.04%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -15,22 +15,36 @@ class Solution(object):
             res+=i[0]
         return res
     
-                
-                
-#         stack = []
-#         for i in range(len(s)):
-#             if stack and (stack[-1][0] == s[i]):
-#                 stack.append((s[i],stack[-1][1]+1))
-                
-#                 if stack[-1][1] == k:
-#                     for j in range(k):
-#                         stack.pop()
-#             else:
-#                 stack.append((s[i],1))
-#         res = ""
-#         for i in stack:
-#             res+=i[0]
-#         return res
+    
+    
+    def candycrush(s):
+        stack = []
+        for i in range(len(s)):
+
+            # check the occurance of the element
+            if stack and stack[-1][0] != s[i] and stack[-1][1] >= 3:
+                for _ in range(stack[-1][1]):
+                    stack.pop()
+
+            # if same element append the element and prev_count +1 in the stack
+            if stack and stack[-1][0] == s[i]:
+                stack.append((s[i], stack[-1][1] + 1))
+
+            #this element is new so append element and count = 1
+            else:
+                stack.append((s[i],1))
+
+        # in the end if the last element on the stack has occurance more then 3 then remove it
+        if stack[-1][1]>=3:
+            for _ in range(stack[-1][1]):
+                stack.pop()
+        res = ""
+        for i in stack:
+            res+=i[0]
+        return res
+
+    # result = candycrush("baaabbcd")
+    # print(result)
         """
         :type s: str
         :type k: int
