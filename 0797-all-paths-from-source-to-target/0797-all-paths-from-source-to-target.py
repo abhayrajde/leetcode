edd2aacc6fa8a6aa51e3bad4f89ab0f1348da693
Abhay Rajde <abrajde@iu.edu>
2022-10-21 15:29:34 -0400

    Time: 202 ms (15.36%), Space: 14.8 MB (63.48%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def allPathsSourceTarget(self, graph):
+        conn_map = defaultdict(list)
+        
+        start = 0
+        end = len(graph)-1
+        
+        for source, target_list in enumerate(graph):
+            for i in target_list:
+                conn_map[source].append(i)
+        
+        res = []
+        curr_path = []
+        def dfs(curr):
+            
+            if curr == end:
+                curr_path.append(curr)
+                res.append(curr_path[:])
+                curr_path.remove(curr)
+                return
+            
+            curr_path.append(curr)
+            #TODO BUSINESS logic
+            for next in conn_map[curr]:
+                dfs(next)
+            curr_path.remove(curr)
+        dfs(0)
+        return res
+        """
+        :type graph: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        
