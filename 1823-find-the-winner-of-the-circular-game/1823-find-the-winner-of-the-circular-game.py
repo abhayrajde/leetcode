e32c09dcc21dd0557b7a332f94380fd344cde22c
Abhay Rajde <abrajde@iu.edu>
2022-11-01 17:59:13 -0400

    Time: 1780 ms (9.85%), Space: 13.4 MB (56.65%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -2,7 +2,6 @@ class Solution(object):
     def findTheWinner(self, n, k):
         stack1 = [i for i in range(1,n+1)]
         stack2 = []
-        # print (stack1)
         while((len(stack1) + len(stack2)) > 1):
             for i in range(k):
                 if i == k-1:
@@ -10,15 +9,13 @@ class Solution(object):
                 else:
                     stack2.append(stack1.pop(0))
                 if not stack1:
-                    stack1,stack2 = stack2,stack1 
-            # stack1.pop(0)
-            # print(stack1)
-            # print(stack2)
+                    stack1,stack2 = stack2,stack1
                 
-        if stack1:
-            return stack1[0]
-        else:
-            return stack2[0]
+        return stack1[0]
+#         if stack1:
+#             return stack1[0]
+#         else:
+#             return stack2[0]
         # return stack1[0] if stack1 else stack2[0]
                 
                 
