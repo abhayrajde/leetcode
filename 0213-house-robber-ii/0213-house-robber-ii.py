24b3ffbc6c54508ab4b90255cfe42a6f85f05564
Abhay Rajde <abrajde@iu.edu>
2022-11-09 21:26:39 -0500

    Time: 15 ms (95.96%), Space: 13.6 MB (15.63%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,27 +1,57 @@
 class Solution(object):
+#     def rob(self, nums):
+#         if len(nums) == 1:
+#             return nums[0]
+        
+#         def dp_tab(nums):
+#             dp = [0]*len(nums)
+
+#             for i in range(len(nums)):
+#                 pick = nums[i]
+
+#                 if i>1:
+#                     pick += dp[i-2]
+#                 not_pick = 0
+#                 if i > 0:
+#                     not_pick += dp[i-1]
+
+#                 dp[i] = max(pick,not_pick)
+
+#             return(dp[-1])
+#         keep_first = dp_tab(nums[:-1])
+#         keep_last = dp_tab(nums[1:])
+#         return max(keep_first,keep_last)
+    
+    #Memoization Technique
     def rob(self, nums):
         if len(nums) == 1:
             return nums[0]
         
-        def dp_tab(nums):
-            dp = [0]*len(nums)
+        def dp_mem(nums):
+            dp = [-1]*len(nums)
 
-            for i in range(len(nums)):
-                pick = nums[i]
 
-                if i>1:
-                    pick += dp[i-2]
-                not_pick = 0
-                if i > 0:
-                    not_pick += dp[i-1]
+            def func(ind):
+                if ind >= len(nums):
+                    return 0
 
-                dp[i] = max(pick,not_pick)
+                if dp[ind] != -1:
+                    return dp[ind]
 
-            return(dp[-1])
-        keep_first = dp_tab(nums[:-1])
-        keep_last = dp_tab(nums[1:])
-        return max(keep_first,keep_last)
+                pick = nums[ind] + func(ind+2)
+
+                not_pick = 0 + func(ind + 1)
+
+                dp[ind] = max(pick, not_pick)
+
+                return dp[ind]
+            res = func(0)
+            return res
         
+        keep_first = dp_mem(nums[:-1])
+        keep_last = dp_mem(nums[1:])
+        return max(keep_first,keep_last)
+    
     
 #     #Memoization Technique
 #     def rob(self, nums):
