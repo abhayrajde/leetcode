614d3d0eb0c7a53dead69d3845bacfbd0297d38f
Abhay Rajde <abrajde@iu.edu>
2022-11-09 20:44:24 -0500

    Time: 38 ms (28.13%), Space: 13.6 MB (6.78%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,26 +1,48 @@
 class Solution(object):
+    
+    #Tabulation Technique - Dynamic Programming
     def rob(self, nums):
         
-        dp = [-1]*len(nums)
+        dp = [0]*len(nums)
         
-        
-        def func(ind):
-            if ind >= len(nums):
-                return 0
+        for i in range(len(nums)):
             
-            if dp[ind] != -1:
-                return dp[ind]
+            pick = nums[i]
+            if i>1:
+                pick += dp[i-2]
+                
+            not_pick = 0
+            if i > 0:
+                not_pick += dp[i-1]
             
-            pick = nums[ind] + func(ind+2)
+            dp[i] = max(pick, not_pick)
             
-            not_pick = 0 + func(ind + 1)
-            
-            dp[ind] = max(pick, not_pick)
-            
-            return dp[ind]
-        res = func(0)
-        return res
-            
+        return dp[-1]
+                
+    
+#         # Memoization Technique - Dynamic Programming
+#         def rob(self, nums):
+
+#         dp = [-1]*len(nums)
+
+
+#         def func(ind):
+#             if ind >= len(nums):
+#                 return 0
+
+#             if dp[ind] != -1:
+#                 return dp[ind]
+
+#             pick = nums[ind] + func(ind+2)
+
+#             not_pick = 0 + func(ind + 1)
+
+#             dp[ind] = max(pick, not_pick)
+
+#             return dp[ind]
+#         res = func(0)
+#         return res
+
         """
         :type nums: List[int]
         :rtype: int
