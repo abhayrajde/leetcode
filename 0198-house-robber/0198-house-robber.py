ab38f3013e301dd1fa6c6f1c60734f16c1061619
Abhay Rajde <abrajde@iu.edu>
2022-11-09 21:28:30 -0500

    Time: 38 ms (28.13%), Space: 13.6 MB (17.21%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -20,28 +20,28 @@ class Solution(object):
         return dp[-1]
                 
     
-#         # Memoization Technique - Dynamic Programming
-#         def rob(self, nums):
+    # Memoization Technique - Dynamic Programming
+    def rob(self, nums):
 
-#         dp = [-1]*len(nums)
+        dp = [-1]*len(nums)
 
 
-#         def func(ind):
-#             if ind >= len(nums):
-#                 return 0
+        def func(ind):
+            if ind >= len(nums):
+                return 0
 
-#             if dp[ind] != -1:
-#                 return dp[ind]
+            if dp[ind] != -1:
+                return dp[ind]
 
-#             pick = nums[ind] + func(ind+2)
+            pick = nums[ind] + func(ind+2)
 
-#             not_pick = 0 + func(ind + 1)
+            not_pick = 0 + func(ind + 1)
 
-#             dp[ind] = max(pick, not_pick)
+            dp[ind] = max(pick, not_pick)
 
-#             return dp[ind]
-#         res = func(0)
-#         return res
+            return dp[ind]
+        res = func(0)
+        return res
 
         """
         :type nums: List[int]
