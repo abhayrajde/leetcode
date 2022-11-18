dbf3cd8c4274d5f806417811dfa43ef71101c44a
Abhay Rajde <abrajde@iu.edu>
2022-11-18 14:04:02 -0500

    Time: 271 ms (5.90%), Space: 15.2 MB (19.35%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -6,46 +6,51 @@ class Solution(object):
         
         #Tabulation
         
-        dp = [[0]*cols]*rows
+#         dp = [[0]*cols]*rows
         
-        for r in range(rows):
-            for c in range(cols):
-                if r == 0 and c == 0:
-                    dp[r][c] = grid[r][c]
-                else:
-                    if r-1 >= 0:
-                        up = grid[r][c] + dp[r-1][c]
-                    else:
-                        up = float("inf")
-                    if c-1 >= 0:
-                        left = grid[r][c] + dp[r][c-1]
-                    else:
-                        left = float("inf")
+#         for r in range(rows):
+#             for c in range(cols):
+#                 if r == 0 and c == 0:
+#                     dp[r][c] = grid[r][c]
+#                 else:
+#                     if r-1 >= 0:
+#                         up = grid[r][c] + dp[r-1][c]
+#                     else:
+#                         up = float("inf")
+#                     if c-1 >= 0:
+#                         left = grid[r][c] + dp[r][c-1]
+#                     else:
+#                         left = float("inf")
 
-                    dp[r][c] = min(left,up)
-        return dp[rows-1][cols-1]
-
-        #         dp = [[-1]*cols]*rows
-#         def dfs(i,j,dp):
-#             # print(dp)
-#             #Base Conditions
-#             if i == 0 and j == 0:
-#                 return grid[i][j]
+#                     dp[r][c] = min(left,up)
+#         return dp[rows-1][cols-1]
+        
+        #Memoization
+        # dp = [[-1]*cols]*rows
+        dp = [[-1 for x in range(cols)] for y in range(rows)]
+        def dfs(i,j,dp):
+            # print(dp)
+            #Base Conditions
+            if i == rows-1 and j == cols-1:
+                dp[i][j] = grid[i][j]
+                return grid[i][j]
             
-#             if i < 0  or j < 0:
-#                 return float("inf")
+            if i >= rows  or j >= cols:
+                return float("inf")
             
-#             if dp[i][j] != -1:
-#                 return dp[i][j]
+            if dp[i][j] != -1:
+                return dp[i][j]
             
-#             up = grid[i][j] + dfs(i-1,j,dp)
-#             left = grid[i][j] + dfs(i,j-1,dp)
+            down = grid[i][j] + dfs(i+1,j,dp)
+            right = grid[i][j] + dfs(i,j+1,dp)
             
-#             dp[i][j] = min(up, left)
+            dp[i][j] = min(down, right)
             
-#             return dp[i][j]
+            return dp[i][j]
             
-#         return dfs(rows-1,cols-1,dp)
+        return dfs(0,0,dp)
+        print(dp)
+        
         """
         :type grid: List[List[int]]
         :rtype: int
