1fef2c2ca324cf672e3275693050ec266cc4f66b
Abhay Rajde <abrajde@iu.edu>
2022-11-20 19:56:31 -0500

    Time: 17 ms (92.75%), Space: 13.5 MB (29.71%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def removePalindromeSub(self, s):
+        if not s: return 0        #empty string 
+        if s == s[::-1]: return 1 #palindrome
+        return 2 
+        
+        """
+        :type s: str
+        :rtype: int
+        """
+        
