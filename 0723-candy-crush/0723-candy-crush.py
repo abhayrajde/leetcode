c2fd8e03a8810bc2d0fb66080f732b042c8fbe9f
Abhay Rajde <abrajde@iu.edu>
2022-11-26 09:21:17 -0500

    Time: 502 ms (16.67%), Space: 13.7 MB (27.56%) - LeetHub

--- 0723-candy-crush/0723-candy-crush.py
+++ 0723-candy-crush/0723-candy-crush.py
@@ -3,9 +3,6 @@ class Solution(object):
         rows = len(board)
         cols = len(board[0])
         while True:
-            # for i in board:
-            #     print(i)    
-            # print(next)
             # 1, Check
             crush = set()
             for r in range(rows):
@@ -24,24 +21,19 @@ class Solution(object):
             for r,c in crush: 
                 board[r][c] = 0
 
-            # 3, Drop
-            
+            # 3, Drop 
             for c in range(cols):
-                count = 0
                 col_ele = []
                 for r in range(rows):
                     if board[r][c] != 0:
                         col_ele.append(board[r][c])
-                    else:
-                        count += 1
+                    
                 for r in range(rows-1,-1,-1):
                     if col_ele:
                         board[r][c] = col_ele[-1]
                         col_ele.pop()
                     else:
-                        board[r][c] = 0
-                    
-                    
+                        board[r][c] = 0  
         return board
         """
         :type board: List[List[int]]
