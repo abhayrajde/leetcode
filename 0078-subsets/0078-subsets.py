8da490b539aeb426dd60f60b954010ac5bb7d956
Abhay Rajde <abrajde@iu.edu>
2022-12-04 13:26:01 -0500

    Time: 29 ms (71.99%), Space: 13.5 MB (69.63%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,15 +1,24 @@
+# Pick - Not Pick Logic
+# create one dfs/recursion function which takes i. current position ii. current subset
+    # if the current position is greater than len(nums):
+        # append current subset to the result
+        #return
+    #recursively call PICK
+    # recursively call NOT PICK
+    
 class Solution(object):
     def subsets(self, nums):
+        # Base Condition
         if not nums:
             return []
+        
         res = []
         curr = []
         def dfs(i,curr):
             if i >= len(nums):
                 res.append(curr[:])
                 return
-            # res.append(curr[:])
-            # print(curr + [nums[i]])
+            
             pick = dfs(i+1,curr + [nums[i]])
             not_pick = dfs(i+1,curr)
         dfs(0,[])
