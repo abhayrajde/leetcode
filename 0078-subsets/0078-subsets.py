1090c0e69d8f990628a14e8c79b176fa8eb577b4
Abhay Rajde <abrajde@iu.edu>
2022-12-04 13:13:24 -0500

    Time: 41 ms (35.62%), Space: 14 MB (5.84%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def subsets(self, nums):
+        if not nums:
+            return []
+        res = []
+        curr = []
+        def dfs(i,curr):
+            if i >= len(nums):
+                res.append(curr[:])
+                return
+            # res.append(curr[:])
+            print(curr + [nums[i]])
+            pick = dfs(i+1,curr + [nums[i]])
+            not_pick = dfs(i+1,curr)
+        dfs(0,[])
+        return res
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
