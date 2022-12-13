bc7a0ae2060aef6a61213c096b4b0139ca473b05
Abhay Rajde <abrajde@iu.edu>
2022-12-13 04:55:58 -0500

    Time: 84 ms (21.97%), Space: 13.6 MB (24.39%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def countAndSay(self, n):
+        s = '1'
+        for i in range(n-1):
+            count = 1
+            temp = []
+            for index in range(1, len(s)):
+                if s[index] == s[index-1]:
+                    count += 1
+                else:
+                    temp.append(str(count))
+                    temp.append(s[index-1])
+                    count = 1
+            temp.append(str(count))
+            temp.append(s[-1])
+            s = ''.join(temp)
+        return s
+        
+        """
+        :type n: int
+        :rtype: str
+        """
+        
