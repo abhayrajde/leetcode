668fefbdb233702c1564a3ff2dd7765a7e465906
Abhay Rajde <abrajde@iu.edu>
2023-08-21 19:16:52 -0400

    Time: 37 ms (66.13%), Space: 15.7 MB (5.77%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        hms = {}
+        hmt = {}
+        
+        #elements count for string s
+        for i in range(len(s)):
+            if(s[i] not in hms):
+                hms[s[i]] = 1
+            else:
+                hms[s[i]] += 1
+        
+        #elements count for string t
+        for i in range(len(t)):
+            if(t[i] not in hmt):
+                hmt[t[i]] = 1
+            else:
+                hmt[t[i]] += 1
+        
+        #compare both the hashmap dictionary
+        if(len(t)>len(s)):
+            s,t = t,s
+            hms,hmt = hmt,hms
+            
+        for i in range(len(s)):
+            if((s[i] not in hmt) or (hms[s[i]] != hmt[s[i]])):
+                return False
+        return True
+    
+        
+        
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
