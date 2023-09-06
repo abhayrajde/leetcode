e365970a8002946819668959f016ed6cd5d67234
Abhay Rajde <abrajde@iu.edu>
2023-09-06 00:56:51 -0400

    Time: 37 ms (67.73%), Space: 14.1 MB (54.71%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,17 +1,6 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
-#         charset = set()
-#         l = 0
-#         res = 0
-        
-#         for r in range(len(s)):
-#             while s[r] in charset:
-#                 charset.remove(s[l])
-#                 l += 1
-#             charset.add(s[r])
-#             res = max(res, r - l + 1)
-#         return res
-    
+
         unique_set = set()
         repeat = 0
         longest_unique_count = 0
@@ -22,7 +11,9 @@ class Solution(object):
             unique_set.add(s[i])
             longest_unique_count = max(longest_unique_count, i - repeat + 1)
         return(longest_unique_count)
-        
+            
+                
+                
         """
         :type s: str
         :rtype: int
